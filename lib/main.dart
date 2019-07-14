d06e7ba27879375eba450ca89323e5c4693aae45
EsioFreitas <esio.gustavo@gmail.com>
2019-07-14 14:41:41 -0300

    Add chat design

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main(){
-  Firestore.instance.collection("teste").document("teste").setData({"teste": "teste"});
+void main() {
+  Firestore.instance
+      .collection("mensagens")
+      .document()
+      .setData({"from": "Daniel", "texto": "Ola!"});
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat app",
+      debugShowCheckedModeBanner: false,
+      home: chatScreen(),
+    );
+  }
+}
+
+class chatScreen extends StatefulWidget {
+  @override
+  _chatScreenState createState() => _chatScreenState();
+}
+
+class _chatScreenState extends State<chatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat App"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: TextComposer(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isCompose = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: <Widget>[
+            Container(
+              child:
+                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isCompose = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              child: IconButton(
+                  icon: Icon(Icons.send), onPressed: _isCompose ? () {} : null),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
