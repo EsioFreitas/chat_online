8ec8bc9f8ef6e6404790bb55d242142bc926aef3
EsioFreitas <esio.gustavo@gmail.com>
2019-07-31 11:26:50 -0300

    Create search suggestioons

--- favoritos_youtube/lib/screens/home.dart
+++ favoritos_youtube/lib/screens/home.dart
@@ -1,8 +1,34 @@
+import 'package:favoritos_youtube/delegates/data_search.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/youtube.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
+          )
+        ],
+      ),
+    );
   }
 }
